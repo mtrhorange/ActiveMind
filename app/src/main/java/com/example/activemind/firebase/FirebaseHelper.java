87cca3753ae75cb418b49093700ec1c8f15f5c43
Benjamin Goh <gohbwj@gmail.com>
2022-03-14 15:24:23 +0800

    Fetch and populate play history data from firestore to calendar

--- app/src/main/java/com/example/activemind/firebase/FirebaseHelper.java
+++ app/src/main/java/com/example/activemind/firebase/FirebaseHelper.java
@@ -1,9 +1,8 @@
 package com.example.activemind.firebase;
 
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 
+import com.applandeo.materialcalendarview.EventDay;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -14,21 +13,24 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class FirebaseHelper {
 
     /**
      * Update game result data for current user
-     * @param gameName name of the game played: [NUMBER; SEQUENCE; WORD]
+     * @param gameName name of the game user played
+     * @param score score attained by user
      */
     public static void updateUserGameData(String gameName, int score) {
         FirebaseUser fbU = FirebaseAuth.getInstance().getCurrentUser();
@@ -39,14 +41,6 @@ public class FirebaseHelper {
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd");
             ZonedDateTime now = ZonedDateTime.now();
             ZonedDateTime sgDateTime = now.withZoneSameInstant(ZoneId.of("Asia/Shanghai"));
-//            System.out.println(dtf.format(sgDateTime));
-//            System.out.println(sgDateTime.getOffset());
-//            System.out.println(sgDateTime.getYear());
-//            System.out.println(sgDateTime.getMonthValue());
-//            System.out.println(sgDateTime.getDayOfMonth());
-//            System.out.println(sgDateTime.getHour());
-//            System.out.println(sgDateTime.getMinute());
-//            System.out.println(sgDateTime.getSecond());
 
             DocumentReference docRef = db.collection("Users").document(fbU.getUid().toString()).collection(gameName).document(dtf.format(sgDateTime));
 
@@ -64,10 +58,28 @@ public class FirebaseHelper {
                         docRef.update("GamesPlayed", FieldValue.increment(1));
                     }
                     else {
+                        //create entry and document fields
                         Map<String, Object> addInfo = new HashMap<>();
                         addInfo.put("HighScore", score);
                         addInfo.put("GamesPlayed", 1);
                         docRef.set(addInfo);
+                    }
+                }
+            });
+
+            //create entry in history collection as well
+            DocumentReference histDocRef = db.collection("Users").document(fbU.getUid().toString()).collection("History").document(dtf.format(sgDateTime));
+
+            histDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    DocumentSnapshot document = task.getResult();
+                    if (!(document.exists())) {
+                        Map<String, Object> addInfo = new HashMap<>();
+                        addInfo.put("Year", sgDateTime.getYear());
+                        addInfo.put("Month", sgDateTime.getMonthValue());
+                        addInfo.put("Day", sgDateTime.getDayOfMonth());
+                        histDocRef.set(addInfo);
                     }
                 }
             });
