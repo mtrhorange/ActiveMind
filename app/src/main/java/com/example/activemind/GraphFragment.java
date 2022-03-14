8fbf55afc9c4ef04f88c46f3f64f52daa9e74050
huangnq96 <huangnq1996@gmail.com>
2022-03-14 15:17:57 +0800

    update graph ui and button

--- app/src/main/java/com/example/activemind/GraphFragment.java
+++ app/src/main/java/com/example/activemind/GraphFragment.java
@@ -49,12 +49,32 @@ public class GraphFragment extends Fragment {
 
     ArrayList<BarChartData> barChartData = new ArrayList<>();
 
-    public void getScore(){
+    public void getNumberScore(){
         barChartData.add(new BarChartData("07-Mar",10));
         barChartData.add(new BarChartData("06-Mar",30));
         barChartData.add(new BarChartData("05-Mar",50));
         barChartData.add(new BarChartData("04-Mar",70));
         barChartData.add(new BarChartData("03-Mar",100));
+        barChartData.add(new BarChartData("02-Mar",95));
+        barChartData.add(new BarChartData("01-Mar",100));
+    }
+
+    public void getSequenceScore(){
+        barChartData.add(new BarChartData("07-Mar",10));
+        barChartData.add(new BarChartData("06-Mar",20));
+        barChartData.add(new BarChartData("05-Mar",30));
+        barChartData.add(new BarChartData("04-Mar",40));
+        barChartData.add(new BarChartData("03-Mar",50));
+        barChartData.add(new BarChartData("02-Mar",65));
+        barChartData.add(new BarChartData("01-Mar",70));
+    }
+
+    public void getWordScore(){
+        barChartData.add(new BarChartData("07-Mar",30));
+        barChartData.add(new BarChartData("06-Mar",40));
+        barChartData.add(new BarChartData("05-Mar",50));
+        barChartData.add(new BarChartData("04-Mar",60));
+        barChartData.add(new BarChartData("03-Mar",70));
         barChartData.add(new BarChartData("02-Mar",95));
         barChartData.add(new BarChartData("01-Mar",100));
     }
@@ -72,7 +92,7 @@ public class GraphFragment extends Fragment {
 
         barEntryArrayList = new ArrayList<>();
         labelNames = new ArrayList<>();
-        getScore();
+        getNumberScore();
 
         for (int i = 0; i < 7; i++){
             String date = barChartData.get(i).getDate();
@@ -90,6 +110,7 @@ public class GraphFragment extends Fragment {
         BarData barData = new BarData(barDataSet);
         barChart.setData(barData);
         barChart.getAxisLeft().setStartAtZero(true);
+        barChart.getAxisLeft().setAxisMaximum(100);
 
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(labelNames));
@@ -107,21 +128,120 @@ public class GraphFragment extends Fragment {
         numberMemoryGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                barChartData.clear();
+                barEntryArrayList.clear();
+                labelNames.clear();
+                getNumberScore();
 
+                for (int i = 0; i < 7; i++){
+                    String date = barChartData.get(i).getDate();
+                    int score = barChartData.get(i).getScore();
+                    barEntryArrayList.add(new BarEntry(i,score));
+                    labelNames.add(date);
+                }
+
+                BarDataSet barDataSet = new BarDataSet(barEntryArrayList, "Game Scores");
+                barDataSet.setColors(ColorTemplate.rgb("673AB7"));
+                barDataSet.setValueTextSize(12);
+                Description description = new Description();
+                description.setText("");
+                barChart.setDescription(description);
+                BarData barData = new BarData(barDataSet);
+                barChart.setData(barData);
+                barChart.getAxisLeft().setStartAtZero(true);
+                barChart.getAxisLeft().setAxisMaximum(100);
+
+                XAxis xAxis = barChart.getXAxis();
+                xAxis.setValueFormatter(new IndexAxisValueFormatter(labelNames));
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawGridLines(false);
+                xAxis.setDrawAxisLine(false);
+                xAxis.setGranularity(1f);
+                xAxis.setLabelCount(labelNames.size());
+                xAxis.setTextSize(12);
+
+                barChart.animateY(1000);
+                barChart.invalidate();
             }
         });
 
         sequenceMemoryGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                barChartData.clear();
+                barEntryArrayList.clear();
+                labelNames.clear();
+                getSequenceScore();
 
+                for (int i = 0; i < 7; i++){
+                    String date = barChartData.get(i).getDate();
+                    int score = barChartData.get(i).getScore();
+                    barEntryArrayList.add(new BarEntry(i,score));
+                    labelNames.add(date);
+                }
+
+                BarDataSet barDataSet = new BarDataSet(barEntryArrayList, "Game Scores");
+                barDataSet.setColors(ColorTemplate.rgb("673AB7"));
+                barDataSet.setValueTextSize(12);
+                Description description = new Description();
+                description.setText("");
+                barChart.setDescription(description);
+                BarData barData = new BarData(barDataSet);
+                barChart.setData(barData);
+                barChart.getAxisLeft().setStartAtZero(true);
+                barChart.getAxisLeft().setAxisMaximum(100);
+
+                XAxis xAxis = barChart.getXAxis();
+                xAxis.setValueFormatter(new IndexAxisValueFormatter(labelNames));
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawGridLines(false);
+                xAxis.setDrawAxisLine(false);
+                xAxis.setGranularity(1f);
+                xAxis.setLabelCount(labelNames.size());
+                xAxis.setTextSize(12);
+
+                barChart.animateY(1000);
+                barChart.invalidate();
             }
         });
 
         wordMemoryGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                barChartData.clear();
+                barEntryArrayList.clear();
+                labelNames.clear();
+                getWordScore();
 
+                for (int i = 0; i < 7; i++){
+                    String date = barChartData.get(i).getDate();
+                    int score = barChartData.get(i).getScore();
+                    barEntryArrayList.add(new BarEntry(i,score));
+                    labelNames.add(date);
+                }
+
+                BarDataSet barDataSet = new BarDataSet(barEntryArrayList, "Game Scores");
+                barDataSet.setColors(ColorTemplate.rgb("673AB7"));
+                barDataSet.setValueTextSize(12);
+                Description description = new Description();
+                description.setText("");
+                barChart.setDescription(description);
+                BarData barData = new BarData(barDataSet);
+                barChart.setData(barData);
+                barChart.getAxisLeft().setStartAtZero(true);
+                barChart.getAxisLeft().setAxisMaximum(100);
+
+                XAxis xAxis = barChart.getXAxis();
+                xAxis.setValueFormatter(new IndexAxisValueFormatter(labelNames));
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawGridLines(false);
+                xAxis.setDrawAxisLine(false);
+                xAxis.setGranularity(1f);
+                xAxis.setLabelCount(labelNames.size());
+                xAxis.setTextSize(12);
+
+                barChart.animateY(1000);
+                barChart.invalidate();
             }
         });
 
